369e63f6480104b1055321ac45590907133bb286
Hemavarni1501 <hemavarni1501@gmail.com>
2025-07-24 20:15:02 +0530

    fix: Configure server for production deployment

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // --- Server Start ---
-const PORT = 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server running on port: ${PORT}`);
 });
